807b1de6f5378492d4a5bb27dc79c7a7c6dbe30a
JoshuaEvan2020 <joshuaevanwilson@gmail.com>
2024-11-29 00:45:46 +0800

    added jump mechanics

--- BetterThanDino/Assets/Scripts/PlayerMovement.cs
+++ BetterThanDino/Assets/Scripts/PlayerMovement.cs
@@ -3,10 +3,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float jumpForce; // Force applied to jump
+    private bool isGrounded; // To check if the player is on the ground
     private Rigidbody2D body;
 
     public float Health, MaxHealth;
     [SerializeField] private HealthbarUI healthBar;
+    [SerializeField] private Transform groundCheck; // Transform to check ground collision
+    [SerializeField] private float groundCheckRadius; // Radius of the ground check
+    [SerializeField] private LayerMask groundLayer;
 
     private void Awake()
     {
@@ -17,6 +22,8 @@ public class PlayerMovement : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
     }
     private void Update()
     {
@@ -27,7 +34,13 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = Vector3.one;
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector3(1, -1, 1);
-        
+            
+        //to jump    
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpForce);
+        }
+
         if(Input.GetKeyDown("x")) {
             SetHealth(-20f);
         }
