e8bbf4c9d99d6e8406e979213c6283e04bfc5824
Christopher Bertrand <c.bertrandtjo@gmail.com>
2024-12-02 07:01:17 +0800

    feat: smarter enemy with damage dealing

--- BetterThanDino/Assets/Scripts/Enemy_Movement.cs
+++ BetterThanDino/Assets/Scripts/Enemy_Movement.cs
@@ -5,20 +5,125 @@ using UnityEngine;
 public class Enemy_Movement : MonoBehaviour
 {
     public float speed;
+    public float attackRange = 2;
+    public float attackCooldown = 2;
+    public float playerDetectRange = 5;
+    public Transform detectionPoint;
+    public LayerMask playerLayer;
+
+    private float attackCooldownTimer;
+    private int facingDirection = -1;
+    private EnemyState enemyState;
+
     private Rigidbody2D rb;
-    public Transform target;
+    private Transform target;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        ChangeState(EnemyState.Idle);
     }
 
     // Update is called once per frame
     void Update()
     {
+        CheckForPlayer();
+        if (attackCooldownTimer > 0)
+        {
+            attackCooldownTimer -= Time.deltaTime;
+        }
+        if (enemyState == EnemyState.Chasing)
+        {
+            Chase();
+        }
+        else if (enemyState == EnemyState.Attacking)
+        {
+            //Do attack
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    void Chase()
+    {
         Vector2 direction = new Vector2(target.position.x - transform.position.x, 0).normalized;
-        
         rb.velocity = direction * speed;
+        if (direction.x > 0 && facingDirection == -1 ||
+            direction.x < 0 && facingDirection == 1)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        facingDirection *= -1; // Toggle facing direction
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+
+    private void CheckForPlayer()
+    {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(detectionPoint.position, playerDetectRange, playerLayer);
+
+        if (hits.Length > 0)
+        {
+            target = hits[0].transform;
+
+            float distanceToTarget = Vector2.Distance(transform.position, target.position);
+
+            if (distanceToTarget <= attackRange && attackCooldownTimer <= 0)
+            {
+                // Attack if within range and cooldown is ready
+                attackCooldownTimer = attackCooldown;
+                ChangeState(EnemyState.Attacking);
+            }
+            else if (distanceToTarget > attackRange)
+            {
+                // Chase if out of attack range
+                ChangeState(EnemyState.Chasing);
+            }
+        }
+        else
+        {
+            // No player detected, return to idle
+            rb.velocity = Vector2.zero;
+            ChangeState(EnemyState.Idle);
+        }
+    }
+
+
+    void ChangeState(EnemyState newState)
+    {
+        //exit the current animation
+        if (enemyState == EnemyState.Idle)
+            anim.SetBool("isIdle", false);
+        else if (enemyState == EnemyState.Chasing)
+            anim.SetBool("isChasing", false);
+        else if (enemyState == EnemyState.Attacking)
+            anim.SetBool("isAttacking", false);
+        //update current animation
+        enemyState = newState;
+
+        //update the current animation
+        if (enemyState == EnemyState.Idle)
+            anim.SetBool("isIdle", true);
+        else if (enemyState == EnemyState.Chasing)
+            anim.SetBool("isChasing", true);
+        else if (enemyState == EnemyState.Attacking)
+            anim.SetBool("isAttacking", true);
+    }
+
+    private void OnDrawGizmosSelected()
+    {   
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(detectionPoint.position, playerDetectRange);
+    }
+    public enum EnemyState
+    {
+        Idle,
+        Chasing,
+        Attacking
     }
 }
