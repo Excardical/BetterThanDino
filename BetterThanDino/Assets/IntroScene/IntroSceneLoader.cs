e252d558dc873473375e85aee41109d4b40fff60
Excardical <fanepco27@gmail.com>
2024-12-02 12:34:01 +0800

    Add: Intro Scene Lore

---
+++ BetterThanDino/Assets/IntroScene/IntroSceneLoader.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class IntroSceneLoader : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        //Only specify the sceneName or sceneBuildIntex will load the scene with the single mode
+        SceneManager.LoadScene("MenuScreen", LoadSceneMode.Single);
+    }
+}
