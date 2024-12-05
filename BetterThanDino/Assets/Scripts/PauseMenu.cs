bd065e38c62e6c7eeff5fac5feb9d5f1b071d108
Christopher Bertrand <c.bertrandtjo@gmail.com>
2024-12-05 12:58:50 +0800

    fix: pause logic fixture

--- BetterThanDino/Assets/Scripts/PauseMenu.cs
+++ BetterThanDino/Assets/Scripts/PauseMenu.cs
@@ -6,46 +6,61 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
-    [SerializeField] GameObject statsMenu;
     [SerializeField] GameObject pauseButton;
+    private bool pauseOpen = false;
+
+    // private void Update()
+    // {
+    //     if (Input.GetButtonDown("TogglePause"))
+    //     {
+    //         Debug.Log("TogglePause button pressed");
+    //         if (!pauseOpen)
+    //         {
+    //             pauseButton.SetActive(false);
+    //             pauseMenu.SetActive(true);
+    //             Time.timeScale = 0;
+    //             pauseOpen = true;
+    //         }
+    //         else
+    //         {
+    //             pauseButton.SetActive(true);
+    
+    //             pauseMenu.SetActive(false);
+    //             Time.timeScale = 1;
+    //             pauseOpen = false;
+    //         }
+    //     }
+    // }
     public void Pause()
     {
         pauseButton.SetActive(false);
         pauseMenu.SetActive(true);
         Time.timeScale = 0;
+        pauseOpen = true;
     }
 
 
     public void Home()
     {
         pauseButton.SetActive(true);
+        pauseMenu.SetActive(false);
         SceneManager.LoadSceneAsync(0);
         Time.timeScale = 1;
-    }
-    public void Stats()
-    {
-        pauseButton.SetActive(false);
-        pauseMenu.SetActive(false);
-        statsMenu.SetActive(true);
-        Time.timeScale = 0;
-    }
-    public void ReturnToPause()
-    {
-        pauseButton.SetActive(false);
-        statsMenu.SetActive(false);
-        pauseMenu.SetActive(true);
-        Time.timeScale = 0;
+        pauseOpen = false;
     }
     public void Resume()
     {
         pauseButton.SetActive(true);
         pauseMenu.SetActive(false);
         Time.timeScale = 1;
+        pauseOpen = false;
     }
     public void Restart()
     {
         pauseButton.SetActive(true);
+        pauseMenu.SetActive(false);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Time.timeScale = 1;
+        pauseOpen = false;
     }
 }
